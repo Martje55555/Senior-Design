150e9a179ccaa8e19e266dd0bcadaad39deac91e
Martje55555 <martje0629@gmail.com>
2022-03-28 11:44:24 -0500

    inital commit for client

---
+++ client/src/components/historical.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import NavBar from "./navBar";
+
+const Historical = () => {
+
+    return(
+        <h1>This is the Historical Page</h1>
+    )
+}
+
+export default Historical;
---
+++ client/src/components/navBar.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/navBar.css'
+
+const NavBar = () => {
+    const Navigate = useNavigate();
+    const [homeActive, setHomeActive] = useState(false);
+    const [historicalActive, setHistoricalActive] = useState(false);
+    const [controlActive, setControlActive] = useState(false);
+
+    const home = () => {
+        Navigate('/home', { replace: true });
+        setHomeActive(!homeActive);
+        setHistoricalActive(false);
+        setControlActive(false);
+    }
+
+    const historical = () => {
+        Navigate('/historical', { replace: true });
+        setHomeActive(false);
+        setHistoricalActive(!historicalActive);
+        setControlActive(false);
+    }
+
+    const control = () => {
+        Navigate('/control', { replace: true });
+        setHomeActive(false);
+        setHistoricalActive(false);
+        setControlActive(!controlActive);
+    }
+
+    return (
+        <div className="ui fluid four item menu ar">
+            <a onClick={() => home()}
+                className={homeActive ? "item active" : "item"}>
+                Home
+            </a>
+
+            <a onClick={() => historical()}
+                className={historicalActive ? "item active" : "item"}>
+                Historical
+            </a>
+
+            <a onClick={() => control()}
+                className={controlActive ? "item active" : "item"}>
+                Control
+            </a>
+
+        </div>
+    )
+}
+
+export default NavBar;
