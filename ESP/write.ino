150ee172ea17cc727e359781f0543b1d5a3ea4d1
Martje55555 <martje0629@gmail.com>
2022-03-31 12:27:11 -0500

    updated db schema and decoder code

--- ESP/write.ino
+++ ESP/write.ino
@@ -1,3 +1,5 @@
+#include <ArduinoJson.h>
+
 #include <ESP8266WiFi.h>
 // #include <FirebaseArduino.h>
 #include <FirebaseESP8266.h>
@@ -6,16 +8,17 @@
 #include <dht.h>
 #include <Arduino_JSON.h>
 
+
 #define dht_apin0 A0 // Analog Pin sensor is connected to
 #define dht_apin1 A1 // Analog Pin sensor is connected to
 
 // Set these to run example.
 #define FIREBASE_HOST "sdtest-849e7-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH "bcvvq4F10J2mjV48FOB05iMZodjffUY9ypHiHAIU"
-#define WIFI_SSID "Alonso7"
-#define WIFI_PASSWORD "rayados10"
+#define WIFI_SSID "HERNANDEZ WIFI"
+#define WIFI_PASSWORD "25000429"
 
-const char *serverName = "http://129.113.58.182/sensor_1";
+const char *serverName = "http://f4f9438a395c2c.lhrtunnel.link/sensor_1";
 
 dht DHT0, DHT1;
 
@@ -53,7 +56,7 @@ void loop()
 
         http.begin(client, serverName);
 
-        StaticJsonBuffer<500> jsonBuffer;
+        StaticJsonBuffer<500> jsonBuffer; //to create json object
 
         // Create the root object
         JsonObject &root = jsonBuffer.createObject();
@@ -62,8 +65,11 @@ void loop()
         root["humidity"] = DHT0.humidity;
 
         http.addHeader("Content-Type", "application/json");
+
+        char json_string[100];
+        root.prettyPrintTo(json_string, sizeof(json_string));
         
-        int httpResponseCode = http.POST(root);
+        int httpResponseCode = http.POST(json_string);
 
         Serial.print("Response Code: ");
         Serial.println(httpResponseCode);
