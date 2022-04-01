88d7580d7a3d03140418d11e326d9c83e361eae9
AlonsoGonzalezGonzalez <alonso.gonzalezgonzalez16@gmail.com>
2022-04-01 12:46:17 -0500

    home screen work

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,11 +1,24 @@
 import React from "react";
 
-import NavBar from "./navBar";
+import '../styles/home.css'
 
 const Home = () => {
 
-    return(
-        <h1>This is the Home Page</h1>
+    return (
+        <div>
+            <h1 className="sample">Status: On/Off</h1>
+            <h2 className="sample">Gathering data: Yes/No</h2>
+            <h3 className="airHumidity">Air Humidity: 40%</h3>
+            <div className="container">
+                <div className="box1"> ola</div>
+                <div className="box2"> ola</div>
+                <div className="box3"> ola</div>
+            </div>
+            
+            
+        </div>
+        
+
     )
 }
 
