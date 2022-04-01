469b3745a4b389b151530a8c25154b2c4c7b229d
TyresseL <tyressel17@gmail.com>
2022-04-01 13:25:48 -0500

    things

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -5,14 +5,26 @@ import '../styles/home.css'
 const Home = () => {
 
     return (
-        <div>
+        <div className="outerContainer">
             <h1 className="sample">Status: On/Off</h1>
             <h2 className="sample">Gathering data: Yes/No</h2>
             <h3 className="airHumidity">Air Humidity: 40%</h3>
             <div className="container">
-                <div className="box1"> ola</div>
-                <div className="box2"> ola</div>
-                <div className="box3"> ola</div>
+                <div className="innerContainer">
+                    <div className="box"> 1</div>
+                    <div className="box"> 2</div>
+                    <div className="box"> 3</div>
+                </div>
+                <div className="innerContainer">
+                    <div className="box"> 4</div>
+                    <div className="box"> 5</div>
+                    <div className="box"> 6</div>
+                </div>
+                <div lassName="innerContainer">
+                    <div className="box"> 7</div>
+                    <div className="box"> 8</div>
+                    <div className="box"> 9</div>
+                </div>
             </div>
             
             
