d52e18d34902f6dbf50b4fb92adc36a8ca46e8e6
AlonsoGonzalezGonzalez <alonso.gonzalezgonzalez16@gmail.com>
2022-04-04 22:55:24 -0500

    control screen progress

--- client/src/components/control.js
+++ client/src/components/control.js
@@ -6,20 +6,33 @@ import '../styles/control.css'
 const Control = () => {
 
     return(
-        <div className="outerContainer">
+        /*<div className="outerContainer">
             <div className="container">   
                 <h1 className="airHumidity">Last Irrigation: Last Irrigation: 3-26-2022 9:30 AM</h1>
                 <h1 className="airHumidity">First Irrigation: 3-27-2022 3:30 PM</h1>
             </div>
-            <div className="containerPadding"></div>
+            <br/>
             <div className="container1">
+            <br/>
                 <button>ON</button>
-                
-            <div className="container1">
                 <button>OFF</button>
             </div>
-            </div>
         </div>
+        */
+       <div className="outer">
+        <div className="container">
+            <h1>Last Irrigation: 3-26-2022 9:30 AM</h1>
+            <h1>First Irrigation: 3-26-2022 9:30 AM</h1>
+            
+        </div>
+        <div className="buttons">
+        
+                <button className="btn1">ON</button>  
+                
+                <button className="btn2">OFF</button>
+        </div>
+        </div>
+        
     )
 }
 
