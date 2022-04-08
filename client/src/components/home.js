6a1e26194883b492de2c848db128386d88d0e515
Jesus Martinez <71607977+Martje55555@users.noreply.github.com>
2022-04-08 17:07:49 -0500

    quick bug fix (#18)
    fixed navbar disappearing due to screen size issue

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -42,33 +42,36 @@ const Home = ({ handleLogout }) => {
     }
     else {
         return (
-            <div className="outerM">
-                <div className="containerGraphM">
-                    <div className="gridContainer">
-                        <div className="box">1</div>
-                        <div className="box">2</div>
-                        <div className="box">3</div>
+            <>
+                <NavBar />
+                <div className="outerM">
+                    <div className="containerGraphM">
+                        <div className="gridContainer">
+                            <div className="box">1</div>
+                            <div className="box">2</div>
+                            <div className="box">3</div>
+                        </div>
+                        <div className="gridContainer">
+                            <div className="box">4</div>
+                            <div className="box">5</div>
+                            <div className="box">6</div>
+                        </div>
+                        <div className="gridContainer">
+                            <div className="box">7</div>
+                            <div className="box">8</div>
+                            <div className="box">9</div>
+                        </div>
                     </div>
-                    <div className="gridContainer">
-                        <div className="box">4</div>
-                        <div className="box">5</div>
-                        <div className="box">6</div>
+                    <div className="containerInfo">
+                        <h1 className="sample">Status: On/Off</h1>
+                        <h1 className="sample">Gathering data: Yes/No</h1>
+                        <h1 className="sample">Weather: 85°F</h1>
+                        <h1 className="sample">Air Humidity: 40%</h1>
                     </div>
-                    <div className="gridContainer">
-                        <div className="box">7</div>
-                        <div className="box">8</div>
-                        <div className="box">9</div>
-                    </div>
+                    <br style={{ marginTop: "50px" }} />
+                    <center><button onClick={handleLogout}>LOGOUT</button></center>
                 </div>
-                <div className="containerInfo">
-                    <h1 className="sample">Status: On/Off</h1>
-                    <h1 className="sample">Gathering data: Yes/No</h1>
-                    <h1 className="sample">Weather: 85°F</h1>
-                    <h1 className="sample">Air Humidity: 40%</h1>
-                </div>
-                <br style={{ marginTop: "50px" }} />
-                <center><button onClick={handleLogout}>LOGOUT</button></center>
-            </div>
+            </>
         )
     }
 }
