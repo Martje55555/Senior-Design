2b96671604fbcec16684931eb0c13947646ed5ff
Martje55555 <martje0629@gmail.com>
2022-04-19 00:59:36 -0500

    mocked react navigate to fix es6 usednavigate error in testing

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import Login from '../App.js';
+import Home from '../components/home.js';
+import {shallow} from 'enzyme';
+
+const mockedUsedNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+   ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUsedNavigate,
+}));
 
 describe('Login component', () => {
+    afterEach(() => {
+        cleanup();
+    });
+
     test('it renders Login form', async () => {
         render(<Login />);
         const userName = screen.queryByText(/Username/i);
@@ -17,4 +29,15 @@ describe('Login component', () => {
         expect(loginBtn).toBeInTheDocument();
         expect(signupBtn).toBeInTheDocument();
     });
+
+    test('it renders Home Screen', async () => {
+        render(<Home />);
+        
+        const status = screen.queryByText(/Status: On/i);
+        const gathering = screen.queryByText(/Gathering data: Yes/i);
+
+        expect(status).toBeInTheDocument();
+        expect(gathering).toBeInTheDocument();
+    });
+
 });
